c2de73bf34d760f99bd628052bdc2758e8660e18
Jshman <jonghp1128@naver.com>
2023-03-22 15:53:13 +0900

    이중 연결 리스트가 제대로 동작하는지 테스트

--- src/LinkedList/MainLinkedList.java
+++ src/LinkedList/MainLinkedList.java
@@ -5,20 +5,21 @@ import java.io.*;
 public class MainLinkedList {
     public static void main(String[] args) throws IOException {
         SinglyLinkedList ll = null;
+        DoublyLinkedList dl = null;
         BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\user\\IdeaProjects\\testProject\\src\\LinkedList\\input.txt"));
 
         String str;
         String[] stringlist;
         int count = 0;
-        while((str = br.readLine()) != null){
-            ll = new SinglyLinkedList();
+        while ((str = br.readLine()) != null) {
+            dl = new DoublyLinkedList();
 
             System.out.printf("[%d번 째 줄 리스트]\n", ++count);
             stringlist = str.split(" ");
             for (String tmp : stringlist) {
-                ll.insert(Integer.parseInt(tmp));
-                ll.print();
+                dl.insert(Integer.parseInt(tmp));
             }
+            dl.printPrevious();
             System.out.println("= = = = = = = = = = = = = =");
         }
     }
