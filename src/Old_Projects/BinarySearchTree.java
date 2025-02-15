4417e0601af549b840d61d4be01a4f1be10057f1
Jshman <jonghp1128@naver.com>
2025-02-15 17:55:31 +0900

    정리

--- src/Old_Projects/BinarySearchTree.java
+++ src/Old_Projects/BinarySearchTree.java
@@ -1,8 +1,46 @@
-package Tree;
-import Tree.Node;
+package Old_Projects;
 
 public class BinarySearchTree {
-    
+    private class Node {
+        private int data;
+        private Node left;
+        private Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+    }
+
     private Node root = null;
     private int size = 0;
 
